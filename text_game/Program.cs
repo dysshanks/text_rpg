19d07136b39887959a35727bf021bda81d9f7610
dysshanks <ryanvdvorst@outlook.com>
2025-02-23 10:51:57 +0100

    innital game

---
+++ text_game/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace text_game
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Welcome to my text based rpg pls insert a name: ");
+            string name = Console.ReadLine();
+            
+            player player = new player();
+            player.name = name;
+            
+            Console.WriteLine($"hello {player.name}");
+        }
+
+        public class player
+        {
+            public string name;
+            public int score;
+            public int lvl;
+            public int hp;
+            public int mp;
+            public int def;
+            public int atk;
+            public int ballance;
+        }
+    }
+}
